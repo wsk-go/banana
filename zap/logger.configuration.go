c566904c540a5aa70a4488ff6424471ca7b1e43f
284569022 <284569022@qq.com>
2023-04-26 12:08:05 +0800

    first commit

--- zap/logger.configuration.go
+++ zap/logger.configuration.go
@@ -2,12 +2,11 @@ package zap
 
 import (
 	"github.com/JackWSK/go-nest/defines"
-	"github.com/JackWSK/go-nest/nest"
 )
 
 func Configuration(config LoggerConfig) *defines.Configuration {
 	return &defines.Configuration{
-		Beans: []*nest.Bean{
+		Beans: []*defines.Bean{
 			{
 				Value: NewLogger(config),
 				Name:  "",
