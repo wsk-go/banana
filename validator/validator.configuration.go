29383f00b673f20ca05729269f4672fa42d976f4
284569022 <284569022@qq.com>
2023-05-05 15:55:59 +0800

    ok

--- validator/validator.configuration.go
+++ validator/validator.configuration.go
@@ -2,17 +2,19 @@ package validator
 
 import "github.com/JackWSK/banana/defines"
 
-func Configuration() *defines.Configuration {
-	validator, err := NewValidator()
-	if err != nil {
-		panic(err)
-	}
-	return &defines.Configuration{
-		Beans: []*defines.Bean{
-			{
-				Value: validator,
-				Name:  "",
+func Configuration() defines.ModuleFunc {
+	return func(application defines.Application) (*defines.Configuration, error) {
+		validator, err := NewValidator()
+		if err != nil {
+			panic(err)
+		}
+		return &defines.Configuration{
+			Beans: []*defines.Bean{
+				{
+					Value: validator,
+					Name:  "",
+				},
 			},
-		},
+		}, nil
 	}
 }
